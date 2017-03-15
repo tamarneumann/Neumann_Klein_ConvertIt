9ad19e67e20c9b2eb43f33accc8ce7d1beeaaa48
tamar neumann <tamar neumann>
2017-03-14 22:00:48 -0400

    started contact form

---
+++ js/contactForm.js
@@ -0,0 +1,11 @@
+var sendEmail = function(){
+	
+	
+	var name = document.getElementById("name").value;
+	
+	if(name == null)
+	{
+		
+		document.getElementById("nameErr").innerHTML = "*amount must be a valid number";
+	}
+};
