cd0ca75fce902703e568462e62f46a257c081f6f
Gittie Klein <gittiekay@gmail.ciom>
2017-03-16 21:23:02 -0400

    header works on contact form

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,35 +1,23 @@
-var sendEmail = function(){
-	
-	
+var sendEmail = function() {
+
 	var formName = document.getElementById("formName").value;
 	var formEmail = document.getElementById("formEmail").value;
 	var formSubject = document.getElementById("formSubject").value;
-	
-	alert(formName);
-	if(formName == null || formEmail == null || formSubject == null)
-	{
-		if(formName == null)
-		{
-			
-			document.getElementById("nameErr").innerHTML = "*required field";
-		}
-		
-		if(formEmail == null)
-		{
-			
-			document.getElementById("emailErr").innerHTML = "*required field";
-		}
-		
-		if(formSubject == null)
-		{
-			
-			document.getElementById("subjectErr").innerHTML = "*required field";
-		}
-	   return false;
+
+	if (formName == null) {
+
+		document.getElementById("nameErr").innerHTML = "*required field";
 	}
-	
-		
-	
-	
-		
+
+	if (formEmail == null) {
+
+		document.getElementById("emailErr").innerHTML = "*required field";
+	}
+
+	if (formSubject == null) {
+
+		document.getElementById("subjectErr").innerHTML = "*required field";
+	}
+	return formName;
+
 };
