2d2c8d8cc5d07ae4f27fc5ccb6389ced8cb677e9
tamar neumann <tamar neumann>
2017-03-21 21:20:52 -0400

    added resizable to contact form

--- js/contactForm.js
+++ js/contactForm.js
@@ -5,8 +5,9 @@ var sendEmail = function() {
 	var formName = document.getElementById("varName").value;
 	var formEmail = document.getElementById("varEmail").value;
 	var formSubject = document.getElementById("varSubject").value;
+	var formMessage = document.getElementById("varMessage").value;
 
-	if(formName.length == 0 || formEmail.length == 0 || formSubject.length == 0)
+	if(formName.length == 0 || formEmail.length == 0 || formSubject.length == 0 || formMessage.length == 0)
 	{	
 		if (formName.length == 0) {
 	
@@ -21,6 +22,11 @@ var sendEmail = function() {
 		if (formSubject.length == 0) {
 	
 			document.getElementById("subjectErr").innerHTML = "*required field";
+		}
+		
+		if (formMessage.length == 0) {
+	
+			document.getElementById("messageErr").innerHTML = "*required field";
 		}
 		return false; 	//form not submitted
 	}
