4d03bc166c4becb1fb053d80363a9bb1bc46350a
Gittie Klein <gittiekay@gmail.ciom>
2017-03-23 19:54:44 -0400

    added contact form modal

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,5 +1,4 @@
-var sendEmail = function() {
-
+function sendEmail() {
 	var formName = document.getElementById("varName").value;
 	var formEmail = document.getElementById("varEmail").value;
 	var formSubject = document.getElementById("varSubject").value;
@@ -10,21 +9,38 @@ var sendEmail = function() {
 	
 			document.getElementById("nameErr").innerHTML = "*required field";
 		}
+		else {
+			document.getElementById("nameErr").innerHTML = "";
+		}
+	
 	
 		if (formEmail.length == 0) {
 	
 			document.getElementById("emailErr").innerHTML = "*required field";
+		}
+		else {
+			document.getElementById("emailErr").innerHTML = "";
 		}
 	
 		if (formSubject.length == 0) {
 	
 			document.getElementById("subjectErr").innerHTML = "*required field";
 		}
+		else {
+			document.getElementById("subjectErr").innerHTML = "";
+		}
+		
 		return false; 	//form not submitted
 	}
 	else
 	{
+		document.getElementById("nameErr").innerHTML = "";
+		document.getElementById("emailErr").innerHTML = "";
+		document.getElementById("subjectErr").innerHTML = "";
+		
 		//everything is filled out - send the email
+		 document.getElementById("modalContact").modal('hide');
+
 		alert("email sent");
 	}
 	
