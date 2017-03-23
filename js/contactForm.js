acb06b17c515bcbb49fb6ec653adb2428feceb69
Gittie Klein <gittiekay@gmail.com>
2017-03-23 19:58:03 -0400

    added modal

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,41 +1,47 @@
-
-
-var sendEmail = function() {
-
+function sendEmail() {
 	var formName = document.getElementById("varName").value;
 	var formEmail = document.getElementById("varEmail").value;
 	var formSubject = document.getElementById("varSubject").value;
-	var formMessage = document.getElementById("varMessage").value;
 
-	if(formName.length == 0 || formEmail.length == 0 || formSubject.length == 0 || formMessage.length == 0)
+	if(formName.length == 0 || formEmail.length == 0 || formSubject.length == 0)
 	{	
 		if (formName.length == 0) {
 	
 			document.getElementById("nameErr").innerHTML = "*required field";
 		}
+		else {
+			document.getElementById("nameErr").innerHTML = "";
+		}
+	
 	
 		if (formEmail.length == 0) {
 	
 			document.getElementById("emailErr").innerHTML = "*required field";
+		}
+		else {
+			document.getElementById("emailErr").innerHTML = "";
 		}
 	
 		if (formSubject.length == 0) {
 	
 			document.getElementById("subjectErr").innerHTML = "*required field";
 		}
-		
-		if (formMessage.length == 0) {
-	
-			document.getElementById("messageErr").innerHTML = "*required field";
+		else {
+			document.getElementById("subjectErr").innerHTML = "";
 		}
+		
 		return false; 	//form not submitted
 	}
 	else
 	{
+		document.getElementById("nameErr").innerHTML = "";
+		document.getElementById("emailErr").innerHTML = "";
+		document.getElementById("subjectErr").innerHTML = "";
+		
+		//everything is filled out - send the email
+		 document.getElementById("modalContact").modal('hide');
 
-       $( "#dialog" ).dialog();
-  		document.getElementById("dialog").style.display = 'block';
-  	
+		alert("email sent");
 	}
 	
 
